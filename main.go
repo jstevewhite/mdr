c69d488f1b7cda7c883000f2198d3ca0a7487d63
Steve White <stwhite@mac.com>
2025-12-16 12:58:35 -0600

    Fixed App icon.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
 )
 
 //go:embed all:frontend/dist
@@ -27,6 +28,13 @@ func main() {
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
+		},
+		Mac: &mac.Options{
+			TitleBar: mac.TitleBarDefault(),
+			About: &mac.AboutInfo{
+				Title:   "mdr",
+				Message: "A cross-platform Markdown viewer",
+			},
 		},
 	})
 
